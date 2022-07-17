fa320b23b928e284c03843af7d07789a5c0d71ea
parwezaamir1 <parwez.aamir777@gmail.com>
2022-07-17 13:41:40 +0530

    Adding guessing game challenge

---
+++ GuessingGameChallenge/Guessing_Game.py
@@ -0,0 +1,39 @@
+import random
+num = random.randint(1,100)
+
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
+guesses = [0]
+while True:
+    guess = int(input("I'm thinking of a number between 1 and 100.\n What is your guess: "))
+    if 1 > guess > 100:
+        print("OUT OF BOUNDS! Please try again ")
+        continue
+
+    # here we compare the player's guess to our number
+    if guess == num:
+        print(f"Congratulation, you guessed it in only {len(guesses)} Guesses!!")
+        break
+
+    # if guess is incorrect, add guess to the list
+    guesses.append(guess)
+
+    # when testing the first guess, guesses[-2]==0, which evaluates to False
+    # and brings us down to the second section
+    if guesses[-2]:
+        if abs(num-guess ) < abs(num-guesses[-2]):
+            print("WARMER")
+        else:
+            print("COLDER")
+
+    else:
+        if abs(num-guess) <=10:
+            print("WARM!")
+        else:
+            print("COLD")
