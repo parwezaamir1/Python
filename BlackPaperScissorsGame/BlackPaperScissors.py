2634355d76db5445b56d1c30b4539b9d94ca58e5
parwezaamir1 <parwez.aamir777@gmail.com>
2023-01-05 20:33:14 +0530

    adding BalckJack game project

--- BlackPaperScissorsGame/BlackPaperScissors.py
+++ BlackPaperScissorsGame/BlackPaperScissors.py
@@ -1,11 +1,11 @@
-import random
+from random import randint
 
 print("Rock.....")
 print("Paper.....")
 print("Scissors.....")
 
-player = input("Player 1, make your move: ")
-rand_num = random.randint(0, 2)
+player = input("Player 1, make your move: ").lower()
+rand_num = randint(0, 2)
 if rand_num == 0:
     computer = "rock"
 elif rand_num == 1:
@@ -13,22 +13,24 @@ elif rand_num == 1:
 else:
     computer = "scissors"
 print(f"Computer plays: {computer}")
+
+
 if player == computer:
     print("It's a tie")
 elif player == "rock":
     if computer == "scissors":
         print("Player wins")
-    elif computer == "paper":
+    else:
         print("computer wins")
 elif player == "paper":
     if computer == "scissors":
         print("Computer wins")
-    elif computer == "rock":
+    else:
         print("Player1 wins")
 elif player == "scissors":
     if computer == "rock":
         print("Computer wins")
-    elif computer == "paper":
+    else:
         print("Player1 wins")
 else:
     print("Something went wrong")
