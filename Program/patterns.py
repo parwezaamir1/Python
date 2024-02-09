3cc0e05f2b2487a91c858134aa4c18f4e9b6f147
parwezaamir1 <parwez.aamir777@gmail.com>
2024-02-09 12:01:04 +0530

    more examples of pattern added

--- Program/patterns.py
+++ Program/patterns.py
@@ -1,3 +1,4 @@
+# Example 1
 """
 *
 **
@@ -7,9 +8,120 @@
 
 """
 
-row = int(input("Enter the number of rows: "))
+# row = int(input("Enter the number of rows: "))
 
+# for i in range(1, row+1):
+#     for j in range(0, i):
+#         print("*", end=' ')
+#     print('\n')
+
+# Example 2
+'''
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+
+# row = int(input("Enter the no. of rows: "))
+# for i in range(1, row+1):
+#     for j in range(1, row+1):
+#         print("*", end=" ")
+#     print()
+
+# Example 3
+'''
+* * * * *
+* * * *
+* * * 
+* *
+*
+'''
+
+# row = int(input("Enter the no. of rows: "))
+# for i in range(1, row+1):
+#     for j in range(i, row+1):
+#         print("*", end=" ")
+#     print()
+
+
+# Example 4
+'''
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+'''
+# row = int(input("Enter the no. of rows: "))
+# for i in range(1, row+1):
+#     for j in range(i, row+1):
+#         print(" ", end=' ')
+    
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+# Example 5
+'''
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+'''
+
+# row = int(input("Enter the no. of rows: "))
+# for i in range(1, row+1):
+#     for j in range(i):
+#         print(" ", end=" ")
+    
+#     for j in range(i, row+1):
+#         print("*", end=" ")
+#     print()
+
+
+# Example 6
+'''
+         *
+       * * *
+     * * * * * 
+   * * * * * * *
+ * * * * * * * * * 
+'''
+
+# row = int(input("Enter the no. of rows: "))
+# for i in range(1, row+1):
+#     for j in range(i,row+1):
+#         print(" ", end=" ")
+    
+#     for j in range(i):
+#         print("*", end=' ')
+    
+#     for j in range(i-1):
+#         print("*", end=' ')
+#     print()
+
+
+# Example 7
+'''
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        * 
+'''
+
+row = int(input("Enter the no. of rows: "))
 for i in range(1, row+1):
-    for j in range(0, i):
+    for j in range(i):
+        print(" ", end=" ")
+    
+    for j in range(i, row+1):
         print("*", end=' ')
-    print('\n')
+    
+    for j in range(i, row):
+        print("*", end=' ')
+    print()
