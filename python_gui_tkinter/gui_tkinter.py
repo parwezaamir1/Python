3c47d1b3b53e26c0d1d704ae91054f0c690ece84
parwezaamir1 <parwez.aamir777@gmail.com>
2024-02-10 19:45:54 +0530

    tkinter python gui

---
+++ python_gui_tkinter/gui_tkinter.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title("Login Form")
+#root.iconbitmap('')   We can change the icon logo
+root.minsize(400, 400)  # make the minsize of the window, we can also use maxsize 
+root.configure(background='#0000FF')
+root.mainloop()
